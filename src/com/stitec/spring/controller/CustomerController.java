a6327c715753ef2ceec38c7f4d5cded8072e0b69
razi <rachidzine@sfr.fr>
2020-09-14 14:57:06 +0200

    Dev CustomerControllet & inject customerDAO

--- src/com/stitec/spring/controller/CustomerController.java
+++ src/com/stitec/spring/controller/CustomerController.java
@@ -1,15 +1,31 @@
 package com.stitec.spring.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.stitec.spring.dao.CustomerDAO;
+import com.stitec.spring.entity.Customer;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 	
+	@Autowired
+	private CustomerDAO customerDAO;
+	
 	@RequestMapping("/list")
 	public String listCustomers(Model theModel) {
+		
+		List<Customer> thecustomers = customerDAO.getCustomers();
+		
+		theModel.addAttribute("customers", thecustomers);
+		
+		
+		
 		return "list-customers";
 	}
 }
